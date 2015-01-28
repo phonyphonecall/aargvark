d07f7449cb32965def63f486f066e05138bb56d0
Scott Hendrickson <scott@dyn-72-33-225-9.uwnet.wisc.edu>
2015-01-28 09:53:49 -0600

    add src

--- src/com/hendwick/aargvark/annotation/Aargvark.java
+++ src/com/hendwick/aargvark/annotation/Aargvark.java
@@ -1,8 +1,12 @@
 package com.hendwick.aargvark.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Created by scott on 1/27/15.
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Aargvark {
     boolean strictOptions() default false;
     boolean extrasAreFatal() default false;
